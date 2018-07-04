2a8ed6d34bd694c62ccf5ee2a378b8ae6d49fef0
Rakesh Barik <rakesh.barik@tavant.com>
2018-07-04 22:01:16 +0530

    ViewModel Layer to survive activity recreation during orientation change.

---
+++ app/src/main/java/io/rakesh/wiki/presentation/CountryInfoViewModel.java
@@ -0,0 +1,38 @@
+package io.rakesh.wiki.presentation;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import io.rakesh.wiki.data.InfoDataRepository;
+import io.rakesh.wiki.model.CountryInfo;
+
+public class CountryInfoViewModel extends AndroidViewModel {
+
+    private LiveData<CountryInfo> countryInfoLiveData;
+
+    public CountryInfoViewModel(@NonNull Application application) {
+        super(application);
+        InfoDataRepository infoDataRepository = InfoDataRepository.getInstance();
+        infoDataRepository.setApplication(application);
+        infoDataRepository.getCountryInfoFromCloud();
+        countryInfoLiveData = infoDataRepository.getCountryInfoFromDb();
+    }
+
+    @NonNull
+    @Override
+    public <T extends Application> T getApplication() {
+        return super.getApplication();
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+    }
+
+    public LiveData<CountryInfo> getCountryInfoLiveData() {
+        return countryInfoLiveData;
+    }
+
+}
