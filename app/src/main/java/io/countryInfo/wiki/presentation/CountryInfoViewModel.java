64fbe3e5144f98841cf4649ca208cff31b94e8e0
Rakesh Barik <rakesh.barik@tavant.com>
2018-07-11 10:12:59 +0530

    Creating MutableLiveData in ViewModel and passing to Activity

--- app/src/main/java/io/countryInfo/wiki/presentation/CountryInfoViewModel.java
+++ app/src/main/java/io/countryInfo/wiki/presentation/CountryInfoViewModel.java
@@ -4,29 +4,41 @@ import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
+import io.countryInfo.wiki.data.GetCountryInfoCallback;
 import io.countryInfo.wiki.data.InfoDataRepository;
+import io.countryInfo.wiki.data.MessageEvent;
 import io.countryInfo.wiki.model.CountryInfo;
+import io.countryInfo.wiki.model.Resource;
+import io.countryInfo.wiki.model.Status;
 
-public class CountryInfoViewModel extends ViewModel {
+public class CountryInfoViewModel extends ViewModel implements GetCountryInfoCallback{
 
-    private MutableLiveData<CountryInfo> countryInfoLiveData;
+    private MutableLiveData<Resource> countryInfoLiveData;
 
     @Override
     protected void onCleared() {
         super.onCleared();
     }
 
-    public LiveData<CountryInfo> getCountryInfoLiveData() {
+    public LiveData<Resource> getCountryInfoLiveData() {
         if(countryInfoLiveData == null){
             countryInfoLiveData = new MutableLiveData<>();
             getCountryInfoFromCloud();
-            countryInfoLiveData = InfoDataRepository.getInstance().getCountryInfo();
         }
         return countryInfoLiveData;
     }
 
     public void getCountryInfoFromCloud() {
-        InfoDataRepository.getInstance().getCountryInfoFromCloud();
+        InfoDataRepository.getInstance().getCountryInfoFromCloud(this);
     }
 
+    @Override
+    public void onCountryInfoFetchSuccess(CountryInfo countryInfo) {
+        countryInfoLiveData.setValue(new Resource(countryInfo, Status.SUCCESS,null));
+    }
+
+    @Override
+    public void onCountryInfoFetchFailure(MessageEvent messageEvent) {
+        countryInfoLiveData.setValue(new Resource(null, Status.FAILURE,messageEvent.getMessage()));
+    }
 }
