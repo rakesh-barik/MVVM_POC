77dd65242b425b7e2c94d363794463580d97168c
Rakesh Barik <rakesh.barik@tavant.com>
2018-07-15 11:50:23 +0530

    -Added Reactive Network Listener to auto reload if information if not loaded yet. -Removed additional loader -Added Snackbar to show network issue(remains visible until network restores)

--- app/src/main/java/io/countryInfo/wiki/presentation/MainActivity.java
+++ app/src/main/java/io/countryInfo/wiki/presentation/MainActivity.java
@@ -2,60 +2,140 @@ package io.countryInfo.wiki.presentation;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
-import android.content.Context;
+import android.content.IntentFilter;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.test.espresso.IdlingResource;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.Toast;
+import android.util.Log;
+
+import java.util.Objects;
 
 import io.countryInfo.wiki.R;
 import io.countryInfo.wiki.model.Resource;
 import io.countryInfo.wiki.model.Status;
+import io.countryInfo.wiki.utils.ConnectivityChangeReceiver;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 
 public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
-    CountryInfoViewModel viewModel;
-    RecyclerView infoRecyclerView;
-    ProgressBar progressBar;
-    CountryInfoAdapter countryInfoAdapter;
-    SwipeRefreshLayout refreshLayout;
-    IdlingResource mIdlingResource;
+    private CountryInfoViewModel viewModel;
+    private CountryInfoAdapter countryInfoAdapter;
+    private SwipeRefreshLayout refreshLayout;
+    private Snackbar messageBar;
+    private ConnectivityChangeReceiver changeReceiver;
+    private boolean isConnectedToNetwork;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        progressBar = findViewById(R.id.progress);
-        progressBar.setVisibility(View.VISIBLE);
-        refreshLayout = findViewById(R.id.pull_to_refresh);
-        refreshLayout.setOnRefreshListener(this);
+        registerNetworkChangeReceiver();
 
-        infoRecyclerView = findViewById(R.id.info_recycler);
-        infoRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        countryInfoAdapter = new CountryInfoAdapter();
-        infoRecyclerView.setAdapter(countryInfoAdapter);
+        initRefreshLayout();
+
+        initRecyclerView();
+
+        initSnackBar();
+
         loadCountryInfo();
     }
 
+    private void initRefreshLayout() {
+        refreshLayout = findViewById(R.id.pull_to_refresh);
+        refreshLayout.setOnRefreshListener(this);
+        refreshLayout.setColorSchemeResources(
+                R.color.colorAccent,
+                R.color.colorPrimary,
+                R.color.colorPrimaryDark
+        );
+    }
+
+    private void initRecyclerView() {
+        RecyclerView infoRecyclerView = findViewById(R.id.info_recycler);
+        infoRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        countryInfoAdapter = new CountryInfoAdapter();
+        infoRecyclerView.setAdapter(countryInfoAdapter);
+    }
+
+    private void initSnackBar() {
+        messageBar = Snackbar.make(findViewById(R.id.container_layout),
+                R.string.check_network_connection, Snackbar.LENGTH_INDEFINITE);
+        messageBar.getView().setBackgroundColor(getResources()
+                .getColor(R.color.colorAccent, null));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ConnectivityChangeReceiver.isConnected()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new io.reactivex.Observer<Boolean>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Boolean isActive) {
+                        Log.d("MainActivity:isConnected:", isActive.toString());
+                        isConnectedToNetwork = isActive;
+                        if (isActive) {
+                            dismissMessageBar();
+                            if (countryInfoAdapter.getItemCount() == 0) {
+                                setLoadingIndicator(true);
+                                viewModel.getCountryInfoFromCloud();
+                            }
+                        } else {
+                            showError();
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
+    private void dismissMessageBar() {
+        messageBar.dismiss();
+    }
+
+    private void registerNetworkChangeReceiver() {
+        changeReceiver = new ConnectivityChangeReceiver();
+        this.registerReceiver(
+                changeReceiver,
+                new IntentFilter(
+                        ConnectivityManager.CONNECTIVITY_ACTION));
+    }
+
     private void loadCountryInfo() {
+        setLoadingIndicator(true);
         viewModel = ViewModelProviders.of(this).get(CountryInfoViewModel.class);
         viewModel.getCountryInfoLiveData().observe(this, new Observer<Resource>() {
             @Override
             public void onChanged(@Nullable Resource resource) {
                 if (resource != null && resource.getStatus() == Status.SUCCESS) {
-                    getSupportActionBar().setTitle(resource.getCountryInfo().getTitle());
+                    Objects.requireNonNull(getSupportActionBar()).setTitle(resource.getCountryInfo().getTitle());
                     countryInfoAdapter.setRows(resource.getCountryInfo().getRows());
+
                     setLoadingIndicator(false);
+                    dismissMessageBar();
                 } else {
                     showError();
                 }
@@ -65,42 +145,34 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
 
     @Override
     public void onRefresh() {
-        if (!isConnectedToNetwork()) {
-            showError();
+        if (isConnectedToNetwork && viewModel != null) {
+            viewModel.onCleared();
+            viewModel.getCountryInfoFromCloud();
+            dismissMessageBar();
         } else {
-            if (viewModel != null) {
-                viewModel.onCleared();
-                viewModel.getCountryInfoFromCloud();
-            }
+            showError();
         }
-
-    }
-
-    private void showError() {
-        setLoadingIndicator(false);
-        Toast.makeText(this, R.string.network_error, Toast.LENGTH_SHORT).show();
-    }
-
-    private void setLoadingIndicator(boolean active) {
-        progressBar.setVisibility(active ? View.VISIBLE : View.INVISIBLE);
-        refreshLayout.setRefreshing(active);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
     }
 
     @Override
     public void onStop() {
         super.onStop();
+        unregisterReceiver(changeReceiver);
     }
 
-    private boolean isConnectedToNetwork() {
-        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        assert connectivityManager != null;
-        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-        return networkInfo != null && networkInfo.isConnected();
+
+    private void showError() {
+        setLoadingIndicator(false);
+        messageBar.show();
+    }
+
+    private void setLoadingIndicator(final boolean active) {
+        refreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                refreshLayout.setRefreshing(active);
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/io/countryInfo/wiki/utils/ConnectivityChangeReceiver.java
@@ -0,0 +1,38 @@
+package io.countryInfo.wiki.utils;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.Subject;
+
+public class ConnectivityChangeReceiver extends BroadcastReceiver{
+
+    private static Subject<Boolean> booleanSubject = PublishSubject.create();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        assert connectivityManager != null;
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        Boolean isActive;
+        if(networkInfo != null) {
+            isActive = networkInfo.isConnected();
+        }else {
+            isActive = false;
+        }
+        booleanSubject.onNext(isActive);
+        Log.d("OnReceive->", isActive.toString());
+    }
+
+    public static Observable<Boolean> isConnected(){
+        return booleanSubject;
+    }
+
+}
